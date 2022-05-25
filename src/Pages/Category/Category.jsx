ea50fbebdf8b143ff69282f63c26eb22021efa12
Alok Shukla <webalok8206@gmail.com>
2022-05-25 12:47:16 +0530

    Async await

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -4,12 +4,22 @@ export default function Category() {
  const [message, setMessage] = useState('Custom message');
 
  useEffect(() => {
+  console.log('Start...')
+  const loadPost = async () => {
+   console.log('Inside loadPost...')
+   let data = await fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(response => response.json())
+    .then(json => json)
+   console.log(('After Fetch', data));
+   return data;
+  }
 
+  console.log(loadPost())
+  console.log('End...')
  }, []);
 
  return (
   <div>
-   {console.log('rerender')}
    <input
     type="text"
     value={message}
