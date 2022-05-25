68c1f03aea9d9d9179883265a32f08423365202d
Alok Shukla <webalok8206@gmail.com>
2022-05-25 12:56:47 +0530

    Async Await Clarification

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -5,18 +5,18 @@ export default function Category() {
 
  useEffect(() => {
   console.log('Start...')
-  const loadPost = async () => {
-   console.log('Inside loadPost...')
-   let data = await fetch('https://jsonplaceholder.typicode.com/todos/')
-    .then(response => response.json())
-    .then(json => json)
-   console.log(('After Fetch', data));
-   return data;
-  }
-
   console.log(loadPost())
   console.log('End...')
  }, []);
+ 
+ const loadPost = async () => {
+  console.log('Inside loadPost...')
+  let data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+   .then(response => response.json())
+   .then(json => json)
+  console.log(('After Fetch', data));
+  return data;
+ }
 
  return (
   <div>
