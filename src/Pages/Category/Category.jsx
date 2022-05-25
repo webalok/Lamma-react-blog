4e26a51875e183ff5850cc690e18b60fed5e006e
Alok Shukla <webalok8206@gmail.com>
2022-05-25 12:20:50 +0530

    Testing Rerendering

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function Category() {
+ const [message, setMessage] = useState('Custom message');
+
+ useEffect(() => {
+
+ }, []);
+
  return (
   <div>
-    Category page
+   {console.log('rerender')}
+   <input
+    type="text"
+    value={message}
+    placeholder="Enter a message"
+    onChange={e => setMessage(e.target.value)}
+   />
+   <p>
+    <strong>{message}</strong>
+   </p>
   </div>
  )
 }
